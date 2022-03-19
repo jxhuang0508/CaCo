69dced1c18bb4e26cf755ded1df582ec1827094c
jxhuang0508 <55238474+jxhuang0508@users.noreply.github.com>
2022-03-19 16:19:48 +0800

    Delete caco directory

--- caco/scripts/caco.sh
+++
@@ -1,5 +0,0 @@
-conda activate caco
-
-python train_caco.py --cfg configs/CaCo.yml
-#python test.py --cfg configs/CaCo.yml
-#python test.py --cfg configs/CaCo_pretrained.yml
--- caco/scripts/train_source_only.sh
+++
@@ -1,6 +0,0 @@
-conda activate caco
-
-python train.py --cfg configs/source_only.yml
-#python test.py --cfg configs/source_only.yml
-#python test.py --cfg configs/source_only_pretrained.yml
-
